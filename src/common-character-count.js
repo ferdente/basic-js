7843a737e8006b8ef9cd9a0e7730dbfc26dc026f
Kirk Sown <kirill-fes@mail.ru>
2023-12-21 03:28:49 +0300

    feat: done 1-4

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const str1 = s1.split('');
+  const str2 = s2.split('');
+
+  for (let i = 0; i < str1.length; i++) {
+    const index = str2.indexOf(str1[i]);
+    if (index !== -1) {
+      str2.splice(index, 1);
+    }
+  }
+
+  return s2.length-str2.length;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const numStr = String(n);
+  let maxNum = 0;
+
+  for (let i = 0; i < numStr.length; i++) {
+    const numWithoutDigit = Number(numStr.slice(0, i) + numStr.slice(i + 1));
+    if (numWithoutDigit > maxNum) {
+      maxNum = numWithoutDigit;
+    }
+  }
+
+  return maxNum;
 }
 
 module.exports = {
